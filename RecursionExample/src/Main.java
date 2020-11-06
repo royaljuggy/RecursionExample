eb800568e9fa5e62b3277bacd66f3084d1b07902
Jacob <dragonte@examaple.com>
2020-11-05 21:57:34 -0500

    First commit

---
+++ RecursionExample/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type a natural number!");
+        long n = sc.nextInt();
+        System.out.println("Factorial: " + factorial(n));
+        System.out.println("Fibonacci number: " + fibonacciWrapper(n));
+    }
+
+    public static long factorial (long n) {
+        if (n == 0) return 1;
+        else return n * factorial(--n);
+    }
+
+    public static long fibonacciWrapper(long n) {
+        return fibonacci(0, 1, n);
+    }
+
+    public static long fibonacci(long n0, long n1, long n) {
+        if (n == 0)  return n0;
+        else return fibonacci (n1, (n0 + n1), --n);
+    }
+}
